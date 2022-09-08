4410190997214d5be1ab5b9136bbe2f26d3606d1
Joan2509 <wjoanmumbi@gmail.com>
2022-09-08 09:02:08 -0700

    the rest

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/*
- * main - prints exact words followed by a new line*/
+/**
+ * bettystyle documentation
+ * main - prints exact words followed by a new line
+ * Return : 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
