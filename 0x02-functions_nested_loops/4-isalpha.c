1252fff33596e54d19355c65e12e0f2c8182e624
Joan2509 <wjoanmumbi@gmail.com>
2022-09-14 06:40:37 -0700

    Checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: Is the int that will use for the argument of the function
+ * Return: Always zero
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= '2') || (c >= 'A' &&& c<= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
