c20bf390887368afdc1c9ba6df8ed27c332aefcb
Joan2509 <wjoanmumbi@gmail.com>
2022-09-15 07:02:54 -0700

    Test for positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests if positive or negative
+ * Return: Always zero when excess
+ */
+int main(void)
+{
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
+	return (0);
+}
